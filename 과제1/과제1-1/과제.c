2f92bb4d3ebb7d55354a2112812b8a39651e35d4
jun2youn5 <jangziyun77@gmail.com>
2026-03-29 16:22:44 +0900

    feat: 1차시 과제

---
+++ 과제1/과제1-1/과제.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d", a % b);
+	return 0;
+}
